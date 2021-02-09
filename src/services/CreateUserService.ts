52b3a39bcc5b3df9be91ad7705ca642f81462e54
Jordan Pacheco <jordanrune@gmail.com>
2021-02-09 09:58:57 -0300

    App

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../errors/AppError';
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +19,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('Email address already registered');
+      throw new AppError('Email address already registered');
     }
 
     const hashedPassword = await hash(password, 8);
